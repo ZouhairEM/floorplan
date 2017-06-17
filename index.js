aa4e82e6b439f234b225a4e4477018c9668cf81e
Unknown <highaeonkratex@gmail.com>
2017-06-17 14:38:59 +0200

    Backup

--- index.js
+++ index.js
@@ -1,32 +1,50 @@
+makeFloorPlan();
 function makeFloorPlan() {
-
     var defaultSettings = {
-        numberofTables: 10, //change to specific number for your amount
-        tablePos: { posX: 300, posY: 100 },
-        tableSize: { tableW: 10, tableH: 12 },
+        numberofTables: 5,  
+        tablePos: {
+            posX: 100,
+            posY: 100
+        },
+        tableSize: {
+            tableW: 10,
+            tableH: 12
+        },
         takenTables: [2, 4], //
-        numberofChairs: 60,
-        chairPos: { posX: 20, posY: 80 },
-        chairSize: { chairW: 1, chairH: 2 },
-        speed: 1000, // [in milliseconds] if you want the tables to fade in
+        numberofChairs: 30,
+        chairPos: {
+            posX: 20,
+            posY: 80
+        },
+        chairSize: {
+            chairW: 1,
+            chairH: 2
+        },
+        speed: 1000, // tables fade in
     };
 
     function createAvailTables() {
         var availableTables = [],
             newTable, tableProp;
         for (var i = 0; i < defaultSettings.numberofTables; i++) {
-
-            newTable = $('<div id="table' + i + '"></div').css({ 'width': defaultSettings.tableSize.tableW + '%', 'height': defaultSettings.tableSize.tableH + '%', 'background-color': 'black' });
-            tableProp = { 'left': defaultSettings.tablePos.posX, 'top': defaultSettings.tablePos.posY, 'position': 'absolute', 'display': 'none' };
-
-            if (defaultSettings.tablePos.posX >= 800) {
-                defaultSettings.tablePos.posX = 300;
+            newTable = $('<div id="table' + i + '"></div').css({
+                'width': defaultSettings.tableSize.tableW + '%',
+                'height': defaultSettings.tableSize.tableH + '%',
+                'background-color': 'black'
+            });
+            tableProp = {
+                'left': defaultSettings.tablePos.posX,
+                'top': defaultSettings.tablePos.posY,
+                'position': 'absolute',
+                'display': 'none'
+            };
+            if (defaultSettings.tablePos.posX >= 400) {
+                defaultSettings.tablePos.posX = 100;
                 defaultSettings.tablePos.posY += 200;
             } else {
                 defaultSettings.tablePos.posX += 200;
             }
-
-            newTable.css(tableProp).appendTo('body').fadeIn(defaultSettings.speed).delay(500, function() {});
+            newTable.css(tableProp).appendTo('body').fadeIn(defaultSettings.speed);
             availableTables.push(newTable);
         }
         return availableTables;
@@ -36,32 +54,32 @@ function makeFloorPlan() {
         var availableChairs = [],
             newChair, chairProp;
         for (var i = 0; i < defaultSettings.numberofChairs; i++) {
-
-            newChair = $('<div class="chair' + i + '"></div').css({ 'width': defaultSettings.chairSize.chairW + '%', 'height': defaultSettings.chairSize.chairH + '%', 'background-color': 'green' });
-            chairProp = { 'left': defaultSettings.chairPos.posX, 'top': defaultSettings.chairPos.posY, 'position': 'absolute', 'display': 'none' };
-
-            if (defaultSettings.chairPos.posX >= 800) {
-                defaultSettings.chairPos.posX = 300;
+            newChair = $('<div class="chair' + i + '"></div').css({
+                'width': defaultSettings.chairSize.chairW + '%',
+                'height': defaultSettings.chairSize.chairH + '%',
+                'background-color': 'green'
+            });
+            chairProp = {
+                'left': defaultSettings.chairPos.posX,
+                'top': defaultSettings.chairPos.posY,
+                'position': 'absolute',
+                'display': 'none'
+            };
+            if (defaultSettings.chairPos.posX >= 400) {
+                defaultSettings.chairPos.posX = 100;
                 defaultSettings.chairPos.posY += 200;
             } else {
                 defaultSettings.chairPos.posX += 200;
             }
-
-
-
-            newChair.css(chairProp).appendTo('body').fadeIn(defaultSettings.speed).delay(500, function() {});
+            newChair.css(chairProp).appendTo('body').fadeIn(defaultSettings.speed);
             availableChairs.push(newChair);
         }
-
         return availableChairs;
     }
-
     var availableTables = createAvailTables();
     var availableChairs = createAvailChairs();
-
-
+    //trim chair arrays into 6 chairs per array
     var chunkSize = 6;
-
     availableChairs = availableChairs.reduce((acc, item, idx) => {
         let group = acc.pop();
         if (group.length == chunkSize) {
@@ -74,27 +92,24 @@ function makeFloorPlan() {
     }, [
         []
     ]);
-
-    console.log(availableChairs[3][2]);
-    // availableChairs[3][2];
-
-    for (var i=0; i<availableChairs.length; i++){
+    //target specific chair(s)
+    /*    for (var i=0; i<availableChairs.length; i++){
     availableChairs[0][i].css({ 'background-color': 'red' });
-    }
-
-
-
+    }*/
     var takenTables;
     var allOthers = availableTables.filter(function(num) {
         return num !== 5;
     });
-    takenTables = allOthers[2];
-
+    takenTables = allOthers[Math.floor(Math.random() * defaultSettings.numberofTables)];
     $("#check").click(function() {
         for (var i = 0; i < availableTables.length; i++) {
-
-            allOthers[i].css({ 'opacity': '0.5' });
-            takenTables.css({ 'opacity': '1' }).fadeIn(defaultSettings.speed);
+            allOthers[i].css({
+                'opacity': '0.5'
+            });
+            takenTables.css({
+                'opacity': '1'
+            }).fadeIn(defaultSettings.speed);
         }
     });
+console.log(takenTables);
 }
